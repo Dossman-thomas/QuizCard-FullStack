270ba13f0bbc38b187a05d76f15b79b4ee641ef0
Dossman-thomas <tdossman.development@gmail.com>
2026-01-06 16:32:41 -0500

    created standard use utility functions for generating response messages, handling passwords, email addresses, and errors.

--- server/utils/index.js
+++ server/utils/index.js
@@ -1,1 +1,13 @@
-export { response } from './response.util.js'; 
+// generic responses
+export { response } from './response.util.js';
+
+// handling passwords
+export { hashPassword } from './hash-pass.util.js';
+export { validatePasswordStrength } from './passwordValidator.util.js';
+
+// handling errors
+export { logServiceError } from './errorLogger.util.js';
+export { createError } from './errorHandler.util.js';
+
+// handling emails
+export { emailRegex } from './regex.util.js';
